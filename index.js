60d185ea848cb8d44404f2fbc9bf3dd10469e61a
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2021-03-13 01:53:34 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.onload = () => {
     setTimeout(() => {
         const splashScreen = document.querySelector('.splash-screen');
         splashScreen.style.display = 'none';
+        document.body.style.overflowY = 'scroll';
         console.log("hello");
     }, 2500);
 }
