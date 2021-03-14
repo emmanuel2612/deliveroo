d798f253fa7e70f81aa68b0045af8a16cd78b3c3
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2021-03-14 19:59:19 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.onload = () => {
 
-   /* setTimeout(() => {
-        const splashScreen = document.querySelector('.splash-screen');
-        splashScreen.style.display = 'none';
-        document.body.style.overflowY = 'scroll';
-        console.log("hello");
-    }, 2500);*/
+     setTimeout(() => {
+         const splashScreen = document.querySelector('.splash-screen');
+         splashScreen.style.display = 'none';
+         document.body.style.overflowY = 'scroll';
+         console.log("hello");
+     }, 2500);
 
 
     // Global Elements
@@ -48,25 +48,155 @@ window.onload = () => {
     const shuffleContainer = document.querySelector('.food-shuffle-container');
 
 
-    shuffleBox.onclick = () =>{
+    shuffleBox.onclick = () => {
         shuffleContainer.style.display = 'flex';
 
         hideMenu();
 
         setTimeout(() => {
             shuffleContainer.style.transform = 'translateY(0%)';
-        },10);
+        }, 10);
 
         headerClose.onclick = () => {
             shuffleContainer.style.transform = 'translateY(100%)';
             showMenu();
         }
 
-   
+
 
     }
 
+
+
+
+    // Resataurants Array 
+
+    const resataurants = [
+
+        nandos = {
+            name: 'Nandos',
+            star_rate: 4.4,
+            text_rate: 'Very Good',
+            category1: 'Chicken',
+            category2: 'Burgers',
+            filling: true
+        },
+
+        mcdonalds = {
+            name: "McDonald's",
+            star_rate: 3.8,
+            text_rate: 'Good',
+            category1: 'Chicken',
+            category2: 'Burgers',
+            filling: false
+        },
+
+        subway = {
+            name: "McDonald's",
+            star_rate: 3.8,
+            text_rate: 'Good',
+            category1: 'American',
+            category2: 'Sandwiches',
+            filling: false
+        },
+
+        enish = {
+            name: 'Enish Nigerian',
+            star: 5.0,
+            text_rate: 'Excellent',
+            category1: 'Chicken',
+            category2: 'African',
+            filling: true
+        },
+
+        som_saa = {
+            name: 'Som Saa Thai',
+            star: 4.0,
+            text_rate: 'Very Good',
+            category1: 'Noodles',
+            category2: 'Thai',
+            filling: false
+        }
+
+
+    ]
+
+
+
+
+
+    // Quiz 
+
+    const questionBoxOne = document.querySelector('#question-box-1');
+    const questionBoxTwo = document.querySelector('#question-box-2');
+    const questionBoxThree = document.querySelector('#question-box-3');
+
+    const quizOptionOne = document.querySelectorAll('.quiz-option-1');
+    const quizOptionTwo = document.querySelectorAll('.quiz-option-2');
+    const quizOptionThree = document.querySelectorAll('.quiz-option-3');
+
+
     
+
+
+
+    for (let index = 0; index < quizOptionOne.length; index++) {
+
+        quizOptionOne[index].onclick = () => {
+
+            questionBoxOne.style.transform = "translateX(-100%)";
+
+            setTimeout(() => {
+                questionBoxOne.style.opacity = "0";
+            }, 20);
+
+
+            questionBoxTwo.style.transform = "translateX(-100%)";
+
+        var width = 10;
+
+        const bar = document.querySelector(".progress");
+        if (width < 100) {
+            width += 30;
+            bar.style.width = width + '%';
+
+        }
+        }
+
+    }
+
+    for (let index = 0; index < quizOptionTwo.length; index++) {
+
+        quizOptionTwo[index].onclick = () => {
+
+            questionBoxTwo.style.transform = "translateX(-200%)";
+
+            setTimeout(() => {
+                questionBoxTwo.style.opacity = "0";
+            }, 20);
+
+            questionBoxThree.style.transform = "translateX(-200%)";
+
+            var width = 40;
+
+        const bar = document.querySelector(".progress");
+        if (width < 100) {
+            width += 30;
+            bar.style.width = width + '%';
+
+        }
+        }
+
+    }
+
+   
+
+
+
+
+
+
+
 
 
 
@@ -84,4 +214,3 @@ window.onload = () => {
 
 
 }
-
